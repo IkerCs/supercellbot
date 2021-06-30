60ade340af83ad0515c6afdf2a93b9e1b4d57f39
ikerc <69134638+IkerCs@users.noreply.github.com>
2021-06-30 11:55:15 -0500

    Actualizando a discord.js-light

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, Intents, Collection } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js-light');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const config = require('../slappey.json');
 const util = require('./utils/json/utils.json');
@@ -6,6 +6,54 @@ const util = require('./utils/json/utils.json');
 const client = new Client({
     intents: Intents.NON_PRIVILEGED,
     allowedMentions: { parse: ['users'] },
+    messageCacheMaxSize: 1,
+    messageCacheLifetime: 1,
+    messageSweepInterval: 60,
+    messageEditHistoryMaxSize: 1,
+    cacheChannels: false,
+    cacheEmojis: false,
+    cacheRoles: true,
+    cachePresences: false,
+    cacheGuilds: true,
+    cacheOverwrites: true,
+    disabledEvents: [
+        'messageUpdate',
+        'messageDelete',
+        'messageDeleteBulk',
+        'messageReactionRemove',
+        'messageReactionRemoveAll',
+        'messageReactionRemoveEmoji',
+        'channelCreate',
+        'channelUpdate',
+        'channelDelete',
+        'channelPinsUpdate',
+        'roleCreate',
+        'roleUpdate',
+        'roleDelete',
+        'inviteCreate',
+        'inviteDelete',
+        'emojiCreate',
+        'emojiUpdate',
+        'emojiDelete',
+        'guildEmojisUpdate',
+        'guildBanAdd',
+        'guildBanRemove',
+        'guildCreate',
+        'guildUpdate',
+        'guildDelete',
+        'guildUnavailable',
+        'guildMemberAdd',
+        'guildMemberUpdate',
+        'guildMemberRemove',
+        'guildIntegrationsUpdate',
+        'presenceUpdate',
+        'typingStart',
+        'userUpdate',
+        'voiceStateUpdate',
+        'webhookUpdate',
+        'shardConnect',
+        'rest',
+    ],
 });
 
 (async () => {
